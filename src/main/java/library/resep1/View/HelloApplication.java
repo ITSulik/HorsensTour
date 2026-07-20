14c8dc640236d34c4fbb270c9b13b7c6436324b5
Valentin Medvetchi <valmedved8@gmail.com>
2026-07-20 15:02:55 +0200

    bug fix

--- src/main/java/library/resep1/View/HelloApplication.java
+++ src/main/java/library/resep1/View/HelloApplication.java
@@ -11,9 +11,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
       FXMLLoader fxmlLoader =
-          new FXMLLoader(HelloApplication.class.getResource("/library/resep1/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+          new FXMLLoader(HelloApplication.class.getResource("/library/resep1/Dashboard.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(
+                getClass().getResource("/library/resep1/styles.css").toExternalForm()
+        );
         stage.setScene(scene);
         stage.show();
     }
